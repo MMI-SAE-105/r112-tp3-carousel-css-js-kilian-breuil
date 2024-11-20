8ef28ee90e841816009f6a6103d9eb21213a64dc
kilian-breuil <150059078+kilian-breuil@users.noreply.github.com>
2024-11-20 09:17:57 +0100

    debut du code js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,33 @@
 // *** CAROUSEL ***
 // TODO
+
+carousel = document.querySelector(".carousel__container");
+carousel.scrollLeft = 520;
+carousel.scrollBy({ left: +260, behavior: "smooth" });
+carousel.scrollBy({ left: -260, behavior: "smooth" });
+
+prevButton = document.querySelector(".carousel__button--prev");
+
+const carousel = document.querySelector(".carousel__container");
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+
+if (carousel) {
+    prevButton.addEventListener("click", () => {
+      const currentScroll = carousel.scrollLeft;
+      carousel.scrollTo({
+        left: currentScroll - itemWidth,
+        behavior: 'smooth',
+      });
+    });
+  
+    nextButton.addEventListener("click", () => {
+      const currentScroll = carousel.scrollLeft;
+      carousel.scrollTo({
+        left: currentScroll + itemWidth,
+        behavior: 'smooth',
+      });
+    });
+  }
+  
